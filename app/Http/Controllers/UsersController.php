d368c180c900ace0c045d072be0eb88d1e66aadc
YeYunRong <3148113255@qq.com>
2022-07-27 20:59:38 +0800

    data:用户个人页面原型

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UsersController extends Controller
+{
+    /**
+     * 处理个人页面显示
+     */
+    public function show(User $user)
+    {
+        return view('users.show',compact('user'));
+    }
+}
