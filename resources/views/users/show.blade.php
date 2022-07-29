37068975788f885a277ab669341f4e7df2464d5d
YeYunRong <3148113255@qq.com>
2022-07-29 20:12:53 +0800

    data:显示个人资料

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,3 +1,4 @@
+{{--个人信息显示--}}
 @extends('layouts.app')
 
 @section('title', $user->name . ' 的个人中心')
@@ -13,10 +14,10 @@
           alt="{{ $user->name }}">
         <div class="card-body">
           <h5><strong>个人简介</strong></h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+          <p>{{ $user->introduction != null ? $user->introduction : "这个人很懒～什么也没写～"}}</p>
           <hr>
           <h5><strong>注册于</strong></h5>
-          <p>January 01 1901</p>
+          <p>{{ $user->created_at->diffForHumans() }}</p>
         </div>
       </div>
     </div>
