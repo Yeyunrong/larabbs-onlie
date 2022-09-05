ce52a37d0bee30afc3c94663e02e30f81eaf3e24
YeYunRong <3148113255@qq.com>
2022-09-05 11:50:57 +0800

    用户管理后台

--- app/Models/User.php
+++ app/Models/User.php
@@ -108,4 +108,34 @@ class User extends Authenticatable implements MustVerifyEmail
         // 将未读标记为已读
         $this->unreadNotifications->markAsRead();
     }
+
+    /**
+     * 管理员页面操作用户 密码重置
+     */
+    public function setPasswordAttribute($value)
+    {
+        // 如果值的长度等于 60，即认为是已经做过加密的情况
+        if (strlen($value) != 60) {
+
+            // 不等于 60，做密码加密处理
+            $value = bcrypt($value);
+        }
+
+        $this->attributes['password'] = $value;
+    }
+
+    /**
+     * 管理员页面操作用户 头像重置
+     */
+    public function setAvatarAttribute($path)
+    {
+        // 如果不是 `http` 子串开头，那就是从后台上传的，需要补全 URL
+        if ( ! Str::startsWith($path, 'http')) {
+
+            // 拼接完整的 URL
+            $path = config('app.url') . "/uploads/images/avatars/$path";
+        }
+
+        $this->attributes['avatar'] = $path;
+    }
 }
